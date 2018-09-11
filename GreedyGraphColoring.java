8cd27ae6c04b062788800d326982fe7e5545f237
Umang Parmar <umangjparmar@gmail.com>
2018-09-12 00:35:20 +0530

    take input from a file

--- GreedyGraphColoring.java
+++ GreedyGraphColoring.java
@@ -3,14 +3,22 @@ import java.io.PrintWriter;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
-import java.io.File; 
+import java.io.FileReader; 
+import java.io.FileNotFoundException; 
 
 public class GreedyGraphColoring {
 
 	public static void main(String[] args) {
 		
-		BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+		BufferedReader reader = null;
 		PrintWriter pw = new PrintWriter(System.out);
+
+		try {
+			reader = new BufferedReader(new FileReader("input.txt"));	
+		}catch(FileNotFoundException e) {
+			pw.println("Please create input.txt file");
+		}
+
 		StringTokenizer inp = null;
 
 		try {
